937eccbfb4ce467e15c5e2b348c1600aef55ee2f
Hélder Peixoto <helderasp@hotmail.com>
2025-05-14 19:51:41 +0200

    Update ib.py
    layout changed

--- ib.py
+++ ib.py
@@ -66,7 +66,7 @@ def extract_polygons_from_feature(feature):
 st.set_page_config(page_title="Radioactive Cloud Viewer", page_icon=":radioactive:", layout="wide")
 
 # create three columns
-col1, col2, col3 = st.columns([5,1,5])
+#col1, col2, col3 = st.columns([5,1,5])
 
 # --------------------------------------------   select nuclear power plant
 # option for nuclear power plant
@@ -195,142 +195,142 @@ try:
     geojson_bytes.write(json.dumps(contour_layer).encode("utf-8"))
     geojson_bytes.seek(0)
 
-    # ============================  COL1 ============================ 
-    # Only show button if polygons were successfully extracted
-    with col1:
+     
         
-        if polygons:
-            # Build GeoJSON
-            contour_layer = {
-                "type": "FeatureCollection",
-                "features": polygons
-            }
+    if polygons:
+        # Build GeoJSON
+        contour_layer = {
+            "type": "FeatureCollection",
+            "features": polygons
+        }
 
-            # Convert to bytes
-            geojson_bytes = BytesIO()
-            geojson_bytes.write(json.dumps(contour_layer).encode("utf-8"))
-            geojson_bytes.seek(0)
+        # Convert to bytes
+        geojson_bytes = BytesIO()
+        geojson_bytes.write(json.dumps(contour_layer).encode("utf-8"))
+        geojson_bytes.seek(0)
 
-            # Show download button in the sidebar
-            st.sidebar.markdown("#### 🗂️ Export KML file")
-            st.sidebar.download_button(
-                label="📥 Download filtered polygons",
-                data=geojson_bytes,
-                file_name=f"simulation_{date_str}.geojson",
-                mime="application/geo+json"
-            )       
-            
-            st.sidebar.header("", divider="gray")
-            
-            # Sidebar legend after the download button
-            st.sidebar.markdown("""
-                <br>
-                <b>Contour Level (mSv)</b>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#FC0200;"></span><span style="margin-left: 8px;">≥ 500</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#FFA000;"></span><span style="margin-left: 8px;">≥ 100</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#FFFF01;"></span><span style="margin-left: 8px;">≥ 50</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#BEFF00;"></span><span style="margin-left: 8px;">≥ 20</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#00A300;"></span><span style="margin-left: 8px;">≥ 6</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#00FF5B;"></span><span style="margin-left: 8px;">≥ 1</span>
-                </div>
-                <div style="display: flex; align-items: center; padding: 5px;">
-                    <span style="display:inline-block; width:20px; height:15px; background-color:#02FDFF;"></span><span style="margin-left: 8px;">≥ 0.1</span>
-                </div>
-            """, unsafe_allow_html=True)
-
+        # Show download button in the sidebar
+        st.sidebar.markdown("#### 🗂️ Export KML file")
+        st.sidebar.download_button(
+            label="📥 Download filtered polygons",
+            data=geojson_bytes,
+            file_name=f"simulation_{date_str}.geojson",
+            mime="application/geo+json"
+        )       
         
+        st.sidebar.header("", divider="gray")
+        
+        # Sidebar legend after the download button
+        st.sidebar.markdown("""
+            <br>
+            <b>Contour Level (mSv)</b>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#FC0200;"></span><span style="margin-left: 8px;">≥ 500</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#FFA000;"></span><span style="margin-left: 8px;">≥ 100</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#FFFF01;"></span><span style="margin-left: 8px;">≥ 50</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#BEFF00;"></span><span style="margin-left: 8px;">≥ 20</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#00A300;"></span><span style="margin-left: 8px;">≥ 6</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#00FF5B;"></span><span style="margin-left: 8px;">≥ 1</span>
+            </div>
+            <div style="display: flex; align-items: center; padding: 5px;">
+                <span style="display:inline-block; width:20px; height:15px; background-color:#02FDFF;"></span><span style="margin-left: 8px;">≥ 0.1</span>
+            </div>
+        """, unsafe_allow_html=True)
 
-        # Display map
-        m = folium.Map(location=[55, 22], zoom_start=4)
-        folium.GeoJson(contour_layer, name="Contours", style_function=style_function).add_to(m)
-        folium.GeoJson(point, name="Source").add_to(m)        
-       
-        # add layer control    
-        folium.LayerControl().add_to(m)
-
-        # title map
-        st.header(f"☢️ Radioactive Cloud Simulation")
-        st.subheader(f":red[_{central_name}_] - :blue[_{date_str}_]")
-        
-        # display map
-        _ = st_folium(m, width=1000, height=600)
-        
-        # additional info
-        st.markdown("This visualisation is based on the simulation of radioactive cloud released from the :red[_{central_name}_] on :blue[_{date_str}_]. For detailed information, maps and data, visit the Intitut Biosphèere web site: https://nrisk.institutbiosphere.ch/in-en.html")
-          
-        
     
-    with col3:
-        #st.header(f"Number of persons affected :red[_Europe_]")
+    # Display map
+    m = folium.Map(location=[55, 22], zoom_start=4)
+    folium.GeoJson(contour_layer, name="Contours", style_function=style_function).add_to(m)
+    folium.GeoJson(point, name="Source").add_to(m)        
+    
+    # add layer control    
+    folium.LayerControl().add_to(m)
+
+    # title map
+    st.header(f"☢️ Radioactive Cloud Simulation")
+    st.subheader(f":red[_{central_name}_] - :blue[_{date_str}_]")
+    
+    # display map
+    _ = st_folium(m, width=1000, height=600)
+
         
-        # display table to download
-        #st.write(table_name)       
+    # Change column names
+    table_name.columns = [
+        "Simulation Date", "≥ 1", "≥ 6", "≥ 20", "≥ 100", "≥ 500", "Collective Dose (persSv)"
+    ]
+    
+    labels_cor = table_name.columns[1:-1]  # Skip first and last column
+    
+    
+    # Loop through rows to find selected date
+    for date_sel in table_name["Simulation Date"]:
+        selected_date = str(date_sel)
+        if date_str in selected_date:
+            # Extract selected row
+            selected_row = table_name.loc[table_name["Simulation Date"] == date_sel]
 
-        # Change column names
-        table_name.columns = [
-            "Simulation Date", "≥ 1", "≥ 6", "≥ 20", "≥ 100", "≥ 500", "Collective Dose (persSv)"
-        ]
-        labels_cor = table_name.columns[1:-1]  # Skip first and last column
+            # Convert values to integers
+            values = [
+                int(str(val).replace(" ", "").replace("\xa0", "")) 
+                for val in selected_row.values[0][1:-1]
+            ]
+
+            # Generate different colors for each bar (you can customize this list)
+            colors = ['#00FF5B', '#00A300', '#BEFF00', '#FFA000', '#FC0200']
+
+            # Ensure colors match the number of bars (fill any excess with a default color if needed)
+            while len(colors) < len(values):
+                colors.append('#02FDFF')  # A default color for extra bars if needed
+
+            st.subheader(f"Number of persons affected by the radioactive cloud in Europe for :blue[_{date_str}_]")
+
+            # Only show button if polygons were successfully extracted
+            #col1, col2 = st.columns([1,1])
+            
+            
         
+            # Display the data as a table
+            chart_data = pd.DataFrame({
+                'Individual dose (mSv)': labels_cor,
+                'Number of persons affected': values
+            })
+            st.write(chart_data)            
+           
+            
+            # Create Plotly interactive bar chart with different colors for each bar
+            fig = go.Figure(data=go.Bar(x=labels_cor, y=values, marker_color=colors))
+            fig.update_layout(
+                xaxis_title="Individual dose (mSv)",
+                yaxis_title="Number of persons affected",
+                xaxis_tickangle=-45
+            )
+            st.plotly_chart(fig)               
+                    
+            break
         
-
-        # Loop through rows to find selected date
-        import plotly.graph_objects as go
-
-        # Loop through rows to find selected date
-        for date_sel in table_name["Simulation Date"]:
-            selected_date = str(date_sel)
-            if date_str in selected_date:
-                # Extract selected row
-                selected_row = table_name.loc[table_name["Simulation Date"] == date_sel]
-
-                # Convert values to integers
-                values = [
-                    int(str(val).replace(" ", "").replace("\xa0", "")) 
-                    for val in selected_row.values[0][1:-1]
-                ]
-
-                # Generate different colors for each bar (you can customize this list)
-                colors = ['#00FF5B', '#00A300', '#BEFF00', '#FFA000', '#FC0200']
-
-                # Ensure colors match the number of bars (fill any excess with a default color if needed)
-                while len(colors) < len(values):
-                    colors.append('#02FDFF')  # A default color for extra bars if needed
-
-                st.write(f"Number of persons affected by the radioactive cloud in Europe")
-
-                # Display the data as a table
-                chart_data = pd.DataFrame({
-                    'Individual dose (mSv)': labels_cor,
-                    'Number of persons affected': values
-                })
-                st.write(chart_data)
-                
-                # Create Plotly interactive bar chart with different colors for each bar
-                fig = go.Figure(data=go.Bar(x=labels_cor, y=values, marker_color=colors))
-                fig.update_layout(
-                    xaxis_title="Individual dose (mSv)",
-                    yaxis_title="Number of persons affected",
-                    xaxis_tickangle=-45
-                )
-
-                st.plotly_chart(fig)               
+    # ==================== TABLE WITH 1096 SIMULATIONS ====================
+    
+    #st.header("", divider="gray")
+    
+    # title table
+    st.subheader(f"Table with the results of 1096 meteorological simulations for :orange[_Europe_] ")
+    
+    # display table to download
+    st.write(table_name) 
+    
+    # additional info
+    st.markdown(f"This visualisation is based on the simulation of radioactive cloud released from the :red[_{central_name}_] on :blue[_{date_str}_]. For detailed information, maps and data, visit the Intitut Biosphèere web site: https://nrisk.institutbiosphere.ch/in-en.html")
           
-                
-                break
-        
-        
                
 finally:
     os.remove(local_kml_path)
